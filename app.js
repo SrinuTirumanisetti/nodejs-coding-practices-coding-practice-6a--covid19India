be1b5ae286f663b3c45be3073c619927480531d3
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-07-01 13:16:10 +0000

    database and server connection with get states api

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "covid19India.db");
+let db = null;
+
+const InitializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log(`server is running on http://localhost:3000`);
+    });
+  } catch (e) {
+    console.log(`DB error ${e.message}`);
+    process.exit(1);
+  }
+};
+
+InitializeDBAndServer();
+
+app.get("/states", async (request, response) => {
+  const getStatesQuery = `select * from state order by state_id;`;
+  const dbStates = await db.all(getStatesQuery);
+  response.send(dbStates);
+});
